e99d09c8b536602a5cdfb600adc46b85b5683483
Sachintha Dissanayake <124299595+SachinthaNimesh@users.noreply.github.com>
2025-06-18 10:16:04 +0530

    Environmental variables added

--- config/configs.ts
+++ config/configs.ts
@@ -1,2 +1,14 @@
 export const API_URL = 'https://87e89eab-95e5-4c0f-8192-7ee0196e1581-dev.e1-us-east-azure.choreoapis.dev/employee-mgmt-system/student-mgmt-server/v1.0';
-// export const API_URL = "http://localhost:8080"
+
+declare global {
+    interface Window {
+        config: {
+            VITE_AZURE_TENANT_ID: string;
+            VITE_AZURE_CLIENT_ID: string;
+            VITE_API_KEY: string;
+        };
+    }
+}
+
+// Example of exporting the config if needed
+export const appConfig = window.config;
