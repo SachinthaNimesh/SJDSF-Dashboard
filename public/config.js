a372ccddbd0e611200c86344b41dad7c1a8cef8c
Sachintha Dissanayake <124299595+SachinthaNimesh@users.noreply.github.com>
2025-06-26 14:44:01 +0530

    Update VITE_API_KEY in config.js for improved security

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.config = {
   VITE_API_KEY:
-    "chk_eyJrZXkiOiJocTAwMWR6ZzhuMnpkcWR6ajFlMDQ4Y2Q5dWJ4dmFlMTc3ZWJpMTBybWd0NTFnamU3NHRnIn0=i3x8NA",
+    "chk_eyJrZXkiOiI3NndzdWJyNDR3OG5xbjk1M3FzMTFwdWQwd2loaGVoeDY3dDJhYTVkeTBtbno4ZGk4aXd3In0=4ygqeA",
   VITE_AZURE_CLIENT_ID: "e29477a8-d53f-43fb-a02c-bacbe4a1714c",
   VITE_AZURE_TENANT_ID: "4c4d3e48-7132-4338-b2f8-2247be35ee76",
 };
