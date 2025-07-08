eac9b65787edcec5130d8bef1e8fa304a98bdc1f
Sachintha Dissanayake <124299595+SachinthaNimesh@users.noreply.github.com>
2025-07-08 17:08:00 +0530

    Update API_URL to point to the correct development server

--- src/config/configs.ts
+++ src/config/configs.ts
@@ -1,4 +1,5 @@
-export const API_URL = "/choreo-apis/sjdsf/server/v1.0";
+export const API_URL =
+  "https://a01f05ac-d5db-4f40-967f-6a0f97a713d2-dev.e1-us-east-azure.preview-dv.choreoapis.dev/sjdsf/server/v1.0";
 // ("/choreo-apis/employee-mgmt-system/student-mgmt-server/v1");
 // https://e4fabb2f-4692-4208-91a3-f373545b57ff-dev.e1-us-east-azure.st.choreoapis.dev/sjdsf/server/v1.0
 declare global {
